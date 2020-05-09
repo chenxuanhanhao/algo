441d7efd0615b097beba1db9d0112ef19261b2de
Jack Ivanov <e601809@gmail.com>
2020-05-09 13:05:22 +0300

    lxc images fix

--- tests/pre-deploy.sh
+++ tests/pre-deploy.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 sysctl net.ipv6.conf.all.disable_ipv6=0
 
 tar xf $HOME/lxc/cache.tar -C / || echo "Didn't extract cache."
-cp -f tests/lxd-bridge /etc/default/lxc-net
+cp -f tests/lxd-bridge /etc/default/lxd-bridge
 cp -f tests/algo.conf /etc/default/algo.conf
 
 export REPOSITORY=${REPOSITORY:-${GITHUB_REPOSITORY}}
@@ -18,12 +18,12 @@ else
   echo -e "#cloud-config\nssh_authorized_keys:\n - $(cat ~/.ssh/id_rsa.pub)" | lxc profile set default user.user-data -
 fi
 
-systemctl restart lxc-net.service lxd-containers.service lxd.service
+systemctl restart lxd-bridge.service lxd-containers.service lxd.service
 
-lxc profile set default raw.lxc lxc.apparmor.profile=unconfined
+lxc profile set default raw.lxc lxc.aa_profile=unconfined
 lxc profile set default security.privileged true
 lxc profile show default
-lxc launch ubuntu:${UBUNTU_VERSION} algo
+lxc launch images:ubuntu/${UBUNTU_VERSION} algo
 
 ip addr
 
